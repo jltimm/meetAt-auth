1ce8d3b7069faa15bc9424a56c8b36646679e46d
jltimm <36246250+jltimm@users.noreply.github.com>
2020-04-04 15:50:45 -0500

    Adding postgres (#8)

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const {Client} = require('pg');
 
 if (process.argv.includes('--start')) {
-  const port = process.env.PORT || 8082;
+  const port = process.env.PORT;
   console.log('Starting server on port ' + port);
+  const client = new Client({
+    connectionString: process.env.PGCONNSTRING,
+  });
+  client.connect();
   app.listen(port);
 }
 
