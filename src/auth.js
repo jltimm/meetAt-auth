c6dcea4cd1fb39df9ebe979385fa16adadea07dc
jltimm <jacobltimm@gmail.com>
2020-04-21 17:16:26 -0500

    Endpoint renaming

--- src/auth.js
+++ src/auth.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use('/v1/logins', require('./api/logins/controller'));
+app.use('/v1/auth', require('./api/auth/controller'));
 
 if (process.argv.includes('--start')) {
   const port = process.env.PORT;
